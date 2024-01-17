fc00a4f43fed4eadf957fbff78fe495a3bb72c4b
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-17 18:23:27 +0200

    np_matmul

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def np_matmul(mat1, mat2):
+    """
+    Function to evaluate matrix multiplication
+    """
+    return mat1 @ mat2
+
---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Defines a fxn that returns the transpose of a 2D matrix
+"""
+
+
+def matrix_transpose(matrix):
+    pass

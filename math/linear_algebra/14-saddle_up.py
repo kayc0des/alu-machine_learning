9759df94006fa6179b8d29652fcca03969902b27
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-17 18:33:56 +0200

    updated

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -8,6 +8,11 @@ Function defines matrix multiplication
 def np_matmul(mat1, mat2):
     """
     Function to evaluate matrix multiplication
+
+    Parameters:
+    - ndarrays
+
+    Returns:
+    - A new matrix equal to mat1 @ mat 2
     """
     return mat1 @ mat2
- 

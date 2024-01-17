887e9e8683fa83f9443c50ebb461c0a931cad29b
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-17 14:40:18 +0200

    pycodestyle

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,16 +5,15 @@ def matrix_shape(matrix):
     shape.append(len(matrix))
 
     def nested_shape(matrix):
+        """ Get shape of nested List"""
         get_nested_shape = set()
         for sub_list in matrix:
             if isinstance(sub_list, list):
                 num_elements = len(sub_list)
                 get_nested_shape.add(num_elements)
                 if isinstance(sub_list[0], list):
-                    get_nested_shape |= nested_shape(sub_list)
-                
+                    get_nested_shape |= nested_shape(sub_list)         
         return get_nested_shape
-        
     sub_shape = list(nested_shape(matrix))
     shape.extend(sub_shape)
     return shape

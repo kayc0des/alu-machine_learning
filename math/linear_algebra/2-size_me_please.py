508ceb25bc06381357df20beac6a25484c29799b
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-17 14:28:20 +0200

    size me please

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    shape = []
+    shape.append(len(matrix))
+
+    def nested_shape(matrix):
+        get_nested_shape = set()
+        for sub_list in matrix:
+            num_elements = len(sub_list)
+            get_nested_shape.add(num_elements)
+            if isinstance(sub_list[0], list):
+                get_nested_shape |= nested_shape(sub_list)
+        return get_nested_shape
+        
+    sub_shape = list(nested_shape(matrix))
+    shape.extend(sub_shape)
+    return shape

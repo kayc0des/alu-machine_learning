73d7b3a394acb6574163c0bc2a4bb0a1add24ec8
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-17 18:31:40 +0200

    np matmul documented

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 import numpy as np
+"""
+Function defines matrix multiplication
+"""
+
 
 def np_matmul(mat1, mat2):
     """
     Function to evaluate matrix multiplication
     """
     return mat1 @ mat2
-
+ 

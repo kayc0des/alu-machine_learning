44ac45b05148115fd90f7bfedb9e51d2b4f6a23e
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-17 14:35:02 +0200

    shape updated

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -6,10 +6,16 @@ def matrix_shape(matrix):
     def nested_shape(matrix):
         get_nested_shape = set()
         for sub_list in matrix:
-            num_elements = len(sub_list)
-            get_nested_shape.add(num_elements)
-            if isinstance(sub_list[0], list):
-                get_nested_shape |= nested_shape(sub_list)
+            if isinstance(sub_list, list):
+                num_elements = len(sub_list)
+                get_nested_shape.add(num_elements)
+                if isinstance(sub_list[0], list):
+                    get_nested_shape |= nested_shape(sub_list)
+            else:
+                # Handle the case where sub_list is not a list (e.g., an integer)
+                num_elements = 1
+                get_nested_shape.add(num_elements)
+                
         return get_nested_shape
         
     sub_shape = list(nested_shape(matrix))

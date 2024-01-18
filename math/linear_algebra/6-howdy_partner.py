0d9294f1e590f357b26b42408fc4bc01040a3dd3
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-18 20:11:31 +0200

    howdy-updated

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -8,5 +8,7 @@ def cat_arrays(arr1, arr2):
     """
     Returns the a concatenated array of both matrices
     """
-    arr3 = arr1.extend(arr2)
+    arr3 = []
+    arr3.extend(arr1)
+    arr3.extend(arr2)
     return arr3

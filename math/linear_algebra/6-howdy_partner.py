136967f9cfe64eef15e8799c361d8587ab2a0111
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-18 19:59:06 +0200

    howdy

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Defines a fxn that concatenates two matrices
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Returns the a concatenated array of both matrices
+    """
+
+    return arr1.extends(arr2)

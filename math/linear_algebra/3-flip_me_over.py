22ed96ae2e961f7c6f95d76c9ee3cf555f6f69f5
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-19 11:13:36 +0200

    flip me over

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -5,4 +5,13 @@ Defines a fxn that returns the transpose of a 2D matrix
 
 
 def matrix_transpose(matrix):
-    pass
+    """
+    Function returns the transpose of a matrix
+    """
+    transpose = []
+    for col in range(len(matrix[0])):
+        temp_matrix = []
+        for row in range(len(matrix)):
+            temp_matrix.append(matrix[row][col])
+        transpose.append(temp_matrix)
+    return transpose

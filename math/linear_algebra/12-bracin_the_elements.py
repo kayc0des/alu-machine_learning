09124b4c3f359c2cb726683e46724d851a03ef5c
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-19 12:35:45 +0200

    12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -5,15 +5,16 @@ This script provides a function to concatenate two matrices along a specified ax
 
 
 def np_elementwise(mat1, mat2):
-    def elementwise_op(func, matrix1, matrix2):
-        return [
-            [func(e1, e2) for e1, e2 in zip(row1, row2)]
-            for row1, row2 in zip(matrix1, matrix2)
-        ]
+    # Element-wise addition
+    add = mat1 + mat2
+    
+    # Element-wise subtraction
+    sub = mat1 - mat2
+    
+    # Element-wise multiplication
+    mul = mat1 * mat2
+    
+    # Element-wise division
+    div = mat1 / mat2
 
-    return (
-        elementwise_op(lambda x, y: x + y, mat1, mat2),
-        elementwise_op(lambda x, y: x - y, mat1, mat2),
-        elementwise_op(lambda x, y: x * y, mat1, mat2),
-        elementwise_op(lambda x, y: x / y, mat1, mat2),
-    )
+    return add, sub, mul, div

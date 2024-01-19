3ed0c99e7703a95b3cb56aa57a4de8b69d97a97a
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-19 12:33:43 +0200

    12 changed

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -5,16 +5,15 @@ This script provides a function to concatenate two matrices along a specified ax
 
 
 def np_elementwise(mat1, mat2):
-    is_list = lambda x: isinstance(x, list)
+    def elementwise_op(func, matrix1, matrix2):
+        return [
+            [func(e1, e2) for e1, e2 in zip(row1, row2)]
+            for row1, row2 in zip(matrix1, matrix2)
+        ]
 
-    add = lambda x, y: x + y
-    sub = lambda x, y: x - y
-    mul = lambda x, y: x * y
-    div = lambda x, y: x / y
-
-    add_result = list(map(add, np_elementwise(mat1[0], mat2[0]))) if is_list(mat1) and is_list(mat2) else mat1 + mat2
-    sub_result = list(map(sub, np_elementwise(mat1[0], mat2[0]))) if is_list(mat1) and is_list(mat2) else mat1 - mat2
-    mul_result = list(map(mul, np_elementwise(mat1[0], mat2[0]))) if is_list(mat1) and is_list(mat2) else mat1 * mat2
-    div_result = list(map(div, np_elementwise(mat1[0], mat2[0]))) if is_list(mat1) and is_list(mat2) else mat1 / mat2
-
-    return add_result, sub_result, mul_result, div_result
+    return (
+        elementwise_op(lambda x, y: x + y, mat1, mat2),
+        elementwise_op(lambda x, y: x - y, mat1, mat2),
+        elementwise_op(lambda x, y: x * y, mat1, mat2),
+        elementwise_op(lambda x, y: x / y, mat1, mat2),
+    )

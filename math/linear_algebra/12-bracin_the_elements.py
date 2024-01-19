701417b9d8db9c15fbfc3e56bc5b1022c3b31c78
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-19 12:40:13 +0200

    12 updated

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """
-This script provides a function to concatenate two matrices along a specified axis
+Script defines a fx to calculate element-wise (+, -, *, /)
 """
 
 
 def np_elementwise(mat1, mat2):
+    """
+    Function returns element-wise +, -, *, / of two matrices
+    """
     # Element-wise addition
     add = mat1 + mat2
-    
     # Element-wise subtraction
     sub = mat1 - mat2
-    
     # Element-wise multiplication
     mul = mat1 * mat2
-    
     # Element-wise division
     div = mat1 / mat2
 

ecd283e33c31b5c28fb31bd3b3ee1b9670a69085
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-01-19 22:25:56 +0200

    102

---
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+This script defines a fn that recursively concatenates two matrices
+"""
+
+
+def cat_matrices(mat1, mat2, axis=0):
+    """
+    Function recursively concatenates two matrices
+    """
+    def recursive_concat(m1, m2, current_axis):
+        if isinstance(m1, list) and isinstance(m2, list):
+            if len(m1) != len(m2):
+                return None
+            if current_axis == axis:
+                return m1 + m2
+            result = []
+            for i in range(len(m1)):
+                sub_result = recursive_concat(m1[i], m2[i], current_axis + 1)
+                if sub_result is None:
+                    return None
+                result.append(sub_result)
+            return result
+        elif current_axis == axis:
+            return None
+        else:
+            return m1 + m2
+
+    result = recursive_concat(mat1, mat2, 0)
+    return result

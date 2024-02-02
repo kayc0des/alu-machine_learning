4b91cc65dab76e15e0cd75fd8873ec5ac6e9e95f
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-02-02 19:25:58 +0200

    log on fire

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+    A function that returns the summation of i**2
+'''
+
+
+def summation_i_squared(n):
+    """
+    Returns the summation of i**2
+    for values of i from 0 to n
+    """
+    if type(n) != 'int':
+        return None
+    
+    

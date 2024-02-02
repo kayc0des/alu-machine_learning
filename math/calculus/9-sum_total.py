a70cf3c8f82e0e12ac1dee7ab3fee6cd9ad48098
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-02-02 20:14:07 +0200

    sum_total

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -9,7 +9,8 @@ def summation_i_squared(n):
     Returns the summation of i**2
     for values of i from 0 to n
     """
-    if type(n) != 'int':
+
+    if type(n) != int:
         return None
-    else:
-        return n * (n + 1) * (2 * n + 1) // 6
+    
+    return n * (n + 1) * (2 * n + 1) // 6

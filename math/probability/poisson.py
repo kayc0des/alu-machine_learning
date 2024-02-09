3a875ca62ea11ce6d8d935ca62f91081e1bd6e60
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-02-09 16:08:24 +0200

    pmf updated

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -35,7 +35,7 @@ class Poisson():
         """ Calculates the value of the PMF for a given number of successes"""
         # assigning the value of euler's number to a variable e
         e = 2.718281828459045
-        if k < 0:
+        if k == 0:
             return 1   
         if not isinstance(k, int):
             k = int(k)

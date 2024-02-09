eca485ed0c42aa8f6457d8572aa7827b6db727fd
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-02-09 14:23:51 +0200

    poisson class updated

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -10,7 +10,7 @@ class Poisson():
     def __init__(self, data=None, lambtha=1.):
         """ initialization method """
         if data is None:
-            if lambtha < 0:
+            if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:

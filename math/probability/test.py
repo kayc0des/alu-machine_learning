373c1b12e4fa835f7a031695cb3ea9cee0d25b82
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-02-10 11:53:48 +0200

    normal

--- math/probability/test.py
+++ math/probability/test.py
@@ -1,16 +1,1 @@
-def fact(num):
-    """ Calculates num!"""
-    values = [num for num in range(1, num+1)]
-    factorial = 1
-    if num == 0:
-        return 1
-    for value in values:
-        factorial *= value
-    return factorial
-
-def comb(n, r):
-    """ calculates the combination nCr"""
-    return fact(n) / (fact(n - r) * fact(r))
-
-print(fact(4))
-print(comb(4, 2))
+# Normal.py alternative solution

a5f31750f32f27e751f3bed5a65a7d922853f043
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-02-24 11:35:05 +0200

    convolve grayscale

--- math/convolutions_and_pooling/convolutions_playground/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/convolutions_playground/0-convolve_grayscale_valid.py
@@ -7,36 +7,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-# image = cv2.imread('/Users/kayc0des/dev/learning/Machine Learning/alu-machine_learning/math/convolutions_and_pooling/convolutions_playground/images/dog.png')
-# image_np = np.array(image)
-# print(f'the numpy image has shape {image_np.shape}')
-
-# #define function
-# def convolve_grayscale_valid(image, kernel):
-#     m = image.shape[2]
-#     h = image.shape[0]
-#     w = image.shape[1]
-
-#     kh, kw = kernel.shape
-
-#     # dimensions of convolved image
-#     conv_height = h - kh + 1
-#     conv_width = w - kw + 1
-    
-#     #initialize the convolved image
-#     convolved_image = np.zeros((conv_height, conv_width, m), dtype=int)
-#     print(f'The shape of the convolved image is {convolved_image.shape}')
-
-#     #iterate over the image input
-#     for i in range(conv_height):
-#         for j in range (conv_width):
-#             shadow_area = image[i:i+kh+1, j:j+kw]
-#             convolved_image[i][j] = np.sum(kernel * shadow_area, axis=(1, 2))
-    
-#     return convolved_image
-
-# print(convolve_grayscale_valid(image, kernel))
-
 kernel = np.array([[1, 0, -1],
                    [1, 0, -1],
                    [1, 0, -1]])

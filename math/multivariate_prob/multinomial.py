d6069571ac3518af335bdc0f29bfb11c503a0316
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-03-14 19:18:16 +0200

    init class

---
+++ math/multivariate_prob/multinomial.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This script represents a Multivariate normal distributiion
+"""
+
+
+# import modules
+import numpy as np
+
+
+class Multinomial():
+    """
+    This class reps a Multivariate Gaussian Distribution
+    """
+
+    def __init__(self, data):
+        """ Init method """
+        if not isinstance(data, np.ndarray) or data.ndim != 2:
+            raise TypeError('data must be a 2D numpy.ndarray')
+        d, n = data.shape
+
+        # ensure data contains multiple data points
+        if n < 2:
+            raise ValueError('data must contain multiple data points')
+
+        # set public instance variables
+        self.mean = np.mean(data, axis=0, keepdims=True)
+        self.cov = np.dot((data - self.mean).T, data - self.mean) / (n - 1)
+ 

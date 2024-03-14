328fe2a76d619eef57a479b1edeeba00b14754b6
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-03-14 14:55:36 +0200

    mean-cov

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -26,18 +26,18 @@ def mean_cov(X):
 
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
-    
+
     # Get the number of data points (n) and the number of dimensions (d)
     n, d = X.shape
-    
+
     # Check if there are multiple data points
     if n < 2:
         raise ValueError("X must contain multiple data points")
-    
+
     # Compute the mean of the data set
     mean = np.mean(X, axis=0, keepdims=True)
-    
+
     # Compute the covariance matrix of the data set
     cov = np.dot((X - mean).T, X - mean) / (n - 1)
-    
+
     return mean, cov

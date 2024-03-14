d512b5c1060a1931e05547849e94b138e1400b2a
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-03-14 14:36:14 +0200

    cov-mean

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -9,9 +9,8 @@ import numpy as np
 
 np.random.seed(0)
 X = np.random.multivariate_normal([12,30,10], [[36, -30, 15],
-                                               [-30, 100, -20],
+                                               [-30,100,-20],
                                                [15, -20, 25]], 10000)
-
 
 def mean_cov(X):
     '''

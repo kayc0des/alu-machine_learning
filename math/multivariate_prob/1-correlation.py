233d917f4b32a2e678e8beda32d5bf407c40d8ac
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-03-14 15:26:12 +0200

    correlation

---
+++ math/multivariate_prob/1-correlation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+This script calculates a correlation matrix
+"""
+
+
+#import necessary modules
+import numpy as np
+
+
+def correlation(C):
+    """
+    Calculates a correlation matrix
+
+    Args:
+        C is a numpy.ndarray containing a correlation matrix
+
+    Returns:
+        A numpy.ndarray containing the correlation matrix
+
+    Raise:
+        TypeError: If C is not a numpy.ndarray
+        ValueError: If C does not have shape(d, d)
+    """
+    # run checks
+    if not isinstance(C, np.ndarray):
+        raise TypeError('C must be a numpy.ndarray')
+
+    d = C.ndim
+
+    if C.shape[0] != d or C.shape[1] != d:
+        raise ValueError('C must be a 2D square matrix')
+    
+    correlation_matrix = np.corrcoef(C, rowvar=False)
+    return correlation_matrix

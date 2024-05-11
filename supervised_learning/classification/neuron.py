65b24de939796fe80d2dfd0662a29c742dc947d6
kayc0des <k.boafo@alustudent.com>
2024-05-11 20:41:19 +0200

    neuron.py updated

--- supervised_learning/classification/neuron.py
+++ supervised_learning/classification/neuron.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+""" This script defines a class Neuron that defines
+a single neuron performing binary classification """
+
+
+import numpy as np
+
+class Neuron():
+    
+    def __init__(self):
+        pass

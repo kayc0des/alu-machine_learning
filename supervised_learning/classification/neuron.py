62dabd0f20573c6ae6a4f14174ecddfaeaab7939
kayc0des <k.boafo@alustudent.com>
2024-05-12 11:41:01 +0200

    neuron class

--- supervised_learning/classification/neuron.py
+++ supervised_learning/classification/neuron.py
@@ -6,6 +6,19 @@ a single neuron performing binary classification """
 import numpy as np
 
 class Neuron():
+    """ Define's a single Neuron """
     
-    def __init__(self):
-        pass
+    # declare public instance attributes
+    W = np.random.randn(0, 1)
+    b = 0
+    A = 0
+    
+    def __init__(self, nx):
+        """ Class Constructor
+        Param: nx -> is the number of input features """
+        
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.nx = nx

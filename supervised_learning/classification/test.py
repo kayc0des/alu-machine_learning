eb79d9f5956fd1bfa4b8a794eeaaa200ac9678ba
kayc0des <k.boafo@alustudent.com>
2024-05-12 18:27:39 +0200

    private neuron updated

---
+++ supervised_learning/classification/test.py
@@ -0,0 +1,13 @@
+''' Test Script '''
+
+class User(object):
+    
+    def __init__(self, name):
+        self.__name = name
+        print(f'My name is {self.__name}')
+        
+    def get_name(self):
+        return self.__name 
+        
+user1 = User("Kingsley")
+print(user1.get_name())

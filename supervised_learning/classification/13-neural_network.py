85f8c6513c39524421cce6e635a878c95a4838ed
kayc0des <k.boafo@alustudent.com>
2024-05-17 09:29:57 +0200

    one pass gd

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -102,7 +102,11 @@ class NeuralNetwork(object):
         self.__b2 = self.__b2 - (alpha * db2)
 
         # Backpropagate the error to compute gradients -> hidden layer
-        dz1 = np.dot(self.__W2.T, dz2) * (A1 * (1 - A1))
+        # dz1 = np.dot(self.__W2.T, dz2) * (A1 * (1 - A1))
+        # dw1 = (1 / m) * np.dot(X, dz1.T)
+        # db1 = (1 / m) * np.sum(dz1, axis=1, keepdims=True)
+
+        dz1 = np.matmul(self.__W2.T, dz2) * (A1 * (1 - A1))
         dw1 = (1 / m) * np.matmul(dz1, X.T)
         db1 = (1 / m) * np.sum(dz1, axis=1, keepdims=True)
 

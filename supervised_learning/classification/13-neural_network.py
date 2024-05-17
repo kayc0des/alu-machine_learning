a5525196f37c7ea6198e4c3a0d4ad47126986734
kayc0des <k.boafo@alustudent.com>
2024-05-17 11:17:40 +0200

    updated 13

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -92,22 +92,33 @@ class NeuralNetwork(object):
         ''' One pass of gradient descent '''
         m = Y.shape[1]
 
-        # Gradient descent -> output layer
         dz2 = A2 - Y
         dw2 = (1 / m) * np.matmul(dz2, A1.T)
         db2 = (1 / m) * np.sum(dz2, axis=1, keepdims=True)
-
-        # Update weights and bias of the output layer
-        self.__W2 = self.__W2 - (alpha * dw2)
-        self.__b2 = self.__b2 - (alpha * db2)
-
-        # Backpropagate the error to compute gradients -> hidden layer
         dz1 = np.matmul(self.__W2.T, dz2) * (A1 * (1 - A1))
         dw1 = (1 / m) * np.matmul(dz1, X.T)
         db1 = (1 / m) * np.sum(dz1, axis=1, keepdims=True)
-
-        # Update weights and bias of the hidden layer
+        self.__W2 = self.__W2 - (alpha * dw2)
+        self.__b2 = self.__b2 - (alpha * db2)
         self.__W1 = self.__W1 - (alpha * dw1)
         self.__b1 = self.__b1 - (alpha * db1)
+
+        # Gradient descent -> output layer
+        # dz2 = A2 - Y
+        # dw2 = (1 / m) * np.dot(A1, dz2.T)
+        # db2 = (1 / m) * np.sum(dz2, axis=1, keepdims=True)
+
+        # # Update weights and bias of the output layer
+        # self.__W2 = self.__W2 - (alpha * dw2.T)
+        # self.__b2 = self.__b2 - (alpha * db2)
+
+        # # Backpropagate the error to compute gradients -> hidden layer
+        # dz1 = np.dot(self.__W2.T, dz2) * (A1 * (1 - A1))
+        # dw1 = (1 / m) * np.dot(X, dz1.T)
+        # db1 = (1 / m) * np.sum(dz1, axis=1, keepdims=True)
+
+        # # Update weights and bias of the hidden layer
+        # self.__W1 = self.__W1 - (alpha * dw1.T)
+        # self.__b1 = self.__b1 - (alpha * db1)
 
         return self.__W1, self.__b1, self.__W2, self.__b2

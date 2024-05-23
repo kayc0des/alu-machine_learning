3a5a86a95f2a65bd7518f8b3be3b1189d4315c69
kayc0des <k.boafo@alustudent.com>
2024-05-23 09:57:43 +0000

    updated

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -20,4 +20,3 @@ def create_placeholders(nx, classes):
     x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
     y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
     return x, y
-

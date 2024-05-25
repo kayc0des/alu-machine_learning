9011d739696f379deb3d9fe3f140fd075dea2fb0
kayc0des <k.boafo@alustudent.com>
2024-05-25 18:38:35 +0200

    done

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -11,11 +11,13 @@ def create_layer(prev, n, activation):
     Returns: the tensor output of the layer
     '''
     # Define the variance scaling initializer with mode "FAN_AVG"
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    initializer = tf.contrib.layers.variance_scaling_initializer(
+            mode="FAN_AVG")
 
     # Create the layer with the specified initializer and activation function
-    layer = tf.layers.Dense(units=n, 
-            activation=activation, kernel_initializer=initializer, name='layer')
+    layer = tf.layers.Dense(units=n,
+                            activation=activation,
+                            kernel_initializer=initializer, name='layer')
 
     # Compute the output of the layer
     output = layer(prev)

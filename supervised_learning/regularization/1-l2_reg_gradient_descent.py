22bff97c536f554cac810de031e0b28239e12403
kayc0des <k.boafo@alustudent.com>
2024-05-31 10:52:15 +0200

    test

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-''' Gradient descent with L2 reg '''
+''' Gradient descent with L2 regularization '''
 
 
 import numpy as np
@@ -19,33 +19,32 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     L -> number of layers in the network
     activation -> output: softmax, hidden: tanh
 
-    Expect:
-    Update weights and biases in place
+    Returns:
+    Updated weights and biases
     '''
 
     m = Y.shape[1]
 
-    grad_cache = {}
+    gradients = {}
 
-    for i in reversed(range(1, L+1)):
-        current_a = cache['A' + str(i)]
-        prev_a = cache['A' + str(i - 1)]
+    for i in reversed(range(1, L + 1)):
+        A_current = cache['A' + str(i)]
+        A_previous = cache['A' + str(i - 1)] if i > 1 else cache['A0']
 
         if i == L:
-            # gradient of loss w.r.t output layer
-            grad_cache['dZ' + str(i)] = current_a - Y
+            dZ = A_current - Y
         else:
-            grad_cache['dA' + str(i)] = np.matmul(
-                weights['W' + str(i + 1)].T, grad_cache['dZ' + str(i + 1)])
-            val = 1 - (current_a * current_a)
-            grad_cache['dZ' + str(i)] = grad_cache['dA' + str(i)] * val
+            dA = np.matmul(weights
+                           ['W' + str(i + 1)].T, gradients['dZ' + str(i + 1)])
+            dZ = dA * (1 - np.square(A_current))
 
-        layer_output_grad = grad_cache['dZ' + str(i)]
-        dW = (np.matmul(layer_output_grad,
-                        prev_a.T) / m) + ((
-                            lambtha / m) * weights['W' + str(i)])
-        dB = np.sum(layer_output_grad, axis=1, keepdims=True) / m
+        gradients['dZ' + str(i)] = dZ
+        gradients['dW' + str(i)] = (1 / m) * np.matmul(
+            dZ, A_previous.T) + (lambtha / m) * weights['W' + str(i)]
+        gradients['db' + str(i)] = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        # update weights and biases
-        weights['W' + str(i)] -= alpha * dW
-        weights['b' + str(i)] -= alpha * dB
+        # Update weights and biases
+        weights['W' + str(i)] -= alpha * gradients['dW' + str(i)]
+        weights['b' + str(i)] -= alpha * gradients['db' + str(i)]
+
+    return weights

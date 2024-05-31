0f4eba9fa25e4f50823aee021063a33f1751deda
kayc0des <k.boafo@alustudent.com>
2024-05-31 12:49:37 +0000

    test file added

---
+++ supervised_learning/regularization/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+'''
+Creates a layer with dropout
+'''
+
+
+import tensorflow as tf
+import numpy as np
+
+def dropout_create_layer(prev, n, activation, keep_prob):
+    '''
+    create a layer with dropout
+
+    Param:
+    prev -> tensor containing output of prev layer
+    n -> number of neurons in the layer
+    activation -> activation fxn
+    keep_prob -> probability that a node will be kept
+
+    Returns:
+    output layer
+    '''
+    initializer = tf.contrib.layers.variance_scaling_initializer(
+        mode="FAN_AVG")
+
+    layer = tf.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer)
+    output = layer(prev)
+    layer_output = tf.nn.dropout(output, keep_prob)
+
+    return layer_output
+
+if __name__ == '__main__':
+    tf.set_random_seed(0)
+    np.random.seed(0)
+    x = tf.placeholder(tf.float32, shape=[None, 784])
+    X = np.random.randint(0, 256, size=(10, 784))
+    a = dropout_create_layer(x, 256, tf.nn.tanh, 0.8)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print(sess.run(a, feed_dict={x: X}))

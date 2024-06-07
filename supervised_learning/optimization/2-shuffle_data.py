7b5a3f2a1759df981406ab5587f76a7f10be100d
kayc0des <k.boafo@alustudent.com>
2024-06-07 15:28:13 +0200

    shuffle data

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -19,9 +19,8 @@ def shuffle_data(X, Y):
     m = X.shape[0]
     permuted_indices = np.random.permutation(m)
 
-    #shuffle both arrays
+    # shuffle both arrays
     shuffled_X = X[permuted_indices]
     shuffled_Y = Y[permuted_indices]
 
     return shuffled_X, shuffled_Y
-    

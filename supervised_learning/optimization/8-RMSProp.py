d87916cfc2fe748d41924788b136f6a6117ef99d
kayc0des <k.boafo@alustudent.com>
2024-06-08 08:33:14 +0200

    8 RMSProp

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+''' Creates Training using RMSProp '''
+
+
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    '''
+    Creates Training op using RMSProp algorithm
+
+    Args:
+    loss -> loss of the network
+    alpha -> learning rate
+    beta2 -> RMSProp weight
+    epsilon -> small number to avoid zero division
+
+    Returns:
+    RMSProp optimization operation
+    '''
+
+    return tf.train.RMSPropOptimizer(
+        alpha, beta2, epsilon).minimize(loss)

76b1e615e96edb1e19b89124dba5f2f78cb19dce
kayc0des <k.boafo@alustudent.com>
2024-06-08 11:06:28 +0200

    alpha updated

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -19,7 +19,7 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
 
     Returns:
     The updated value of alpha'''
-    
+
     # Calculate the number of times the learning rate should decay
     decay_steps = global_step // decay_step
 

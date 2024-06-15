d02521abea1ef68ae74607a8078b9ef941320beb
kayc0des <k.boafo@alustudent.com>
2024-06-15 16:10:09 +0200

    error analysis

---
+++ supervised_learning/error_analysis/3-specificity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Specificity"""
+
+import numpy as np
+
+def specificity(confusion):
+    """Calculating the specificity for each class in a confusion matrix"""
+    TP = np.diag(confusion)
+    FN = np.sum(confusion, axis=0) - TP
+    FP = np.sum(confusion, axis=0) - TP
+    TN = np.sum(confusion) - (TP + FN + FP)
+    return TN / (TN + FP)

3f30ec159e346b43d38fad6660d56a580d8c5427
kayc0des <k.boafo@alustudent.com>
2024-06-21 19:55:44 +0200

    4-array

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 
-df = pd.read_csv('data/bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv')
+df = pd.read_csv('data/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv')
 
 # Rename a column using .rename
 df = df.rename(columns={'Timestamp': 'Datetime'})
@@ -12,4 +12,4 @@ df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
 
 df = df[['Datetime', 'Close']]
 
-print(df)
+print(df.tail())

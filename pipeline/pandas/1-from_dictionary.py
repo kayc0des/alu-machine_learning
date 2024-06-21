ada1583c573b6b92ac9312105f69b704735a0d50
kayc0des <k.boafo@alustudent.com>
2024-06-21 17:33:01 +0200

    1

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' Creates dataframe from a dictionary '''
+
+import pandas as pd
+
+dictionary = {
+    'First': [0.0, 0.5, 1.0, 1.5],
+    'Second': ['one', 'two', 'three', 'four']
+}
+
+df = pd.DataFrame(dictionary, index=list('ABCD'))

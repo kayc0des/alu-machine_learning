b686e8b1f62935370c70a85942f8b9f6deee6478
kayc0des <k.boafo@alustudent.com>
2024-06-29 12:49:52 +0200

    sentience

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -1,1 +1,47 @@
 #!/usr/bin/env python3
+'''
+Create a method that returns a list of
+names of home planets of all sentient
+species
+'''
+
+
+import requests
+
+
+def sentientPlanets():
+    '''
+    Returns home planet list
+    '''
+
+    planets = []
+
+    try:
+        url = "https://swapi-api.alx-tools.com/api/species/"
+
+        while url:
+            response = requests.get(url)
+            
+            data = response.json()
+            species = data['results']
+
+            for specie in species:
+                if specie['designation'] == 'sentient':
+                    homeworld_url = specie['homeworld']
+                    if homeworld_url:
+                        homeworld_response = requests.get(homeworld_url)
+                        homeworld_response.raise_for_status()
+                        homeworld_data = homeworld_response.json()
+                        planets.append(homeworld_data['name'])
+
+            url = data.get('next')
+
+        return planets
+
+    except requests.RequestException as e:
+        print('An error occured: {}'.format(e))
+    except Exception as err:
+        print('A general error: {}'.format(err))
+
+planets = sentientPlanets()
+print(planets)
--- pipeline/apis/test.py
+++ pipeline/apis/test.py
@@ -1,5 +1,11 @@
+import requests
 
-number = '895678'
-number = number.replace(',', '')
-print(int(number))
+url = 'https://swapi-api.alx-tools.com/api/species/'
 
+response = requests.get(url)
+
+data = response.json()
+
+species = data['results']
+
+print(species[0])

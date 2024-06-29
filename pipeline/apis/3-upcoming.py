490224a4ea661a70a2a19a6fa6c83097ddf7eada
kayc0des <k.boafo@alustudent.com>
2024-06-29 14:12:49 +0200

    upcoming

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -1,1 +1,58 @@
 #!/usr/bin/env python3
+'''
+Upcoming Launches
+'''
+
+
+import requests
+import datetime
+
+
+def get_upcoming_launch():
+    url = 'https://api.spacexdata.com/v4/launches/upcoming'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        launches = response.json()
+        
+        # Sort launches by date_unix to get the soonest one
+        upcoming_launch = sorted(launches, key=lambda x: x['date_unix'])[0]
+        
+        # Get rocket details
+        rocket_id = upcoming_launch['rocket']
+        rocket_url = 'https://api.spacexdata.com/v4/rockets/{}'.format(
+            rocket_id)
+        rocket_response = requests.get(rocket_url)
+        rocket_response.raise_for_status()
+        rocket = rocket_response.json()
+        rocket_name = rocket['name']
+        
+        # Get launchpad details
+        launchpad_id = upcoming_launch['launchpad']
+        launchpad_url = 'https://api.spacexdata.com/v4/launchpads/{}'.format(
+            launchpad_id)
+        launchpad_response = requests.get(launchpad_url)
+        launchpad_response.raise_for_status()
+        launchpad = launchpad_response.json()
+        launchpad_name = launchpad['name']
+        launchpad_locality = launchpad['locality']
+        
+        # Format date to local time
+        date_local = datetime.datetime.strptime(
+            upcoming_launch['date_local'], '%Y-%m-%dT%H:%M:%S%z')
+        formatted_date = date_local.strftime('%Y-%m-%d %H:%M:%S %Z')
+
+        # Print launch details in the required format
+        print(
+            "{} ({}) {} - {} ({})".format(
+                upcoming_launch['name'], formatted_date, rocket_name,
+                launchpad_name, launchpad_locality))
+    
+    except requests.RequestException as e:
+        print('An error occurred while making an API request: {}'.format(e))
+    except Exception as err:
+        print('A general error occurred: {}'.format(err))
+
+
+if __name__ == '__main__':
+    get_upcoming_launch()
--- pipeline/apis/test.py
+++ pipeline/apis/test.py
@@ -1,9 +1,43 @@
+#!/usr/bin/env python3
 import requests
+import datetime
 
-url = 'https://api.github.com/users/kayc0des'
+def get_upcoming_launch():
+    url = 'https://api.spacexdata.com/v4/launches/upcoming'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        launches = response.json()
+        
+        # Sort launches by date_unix to get the soonest one
+        upcoming_launch = sorted(launches, key=lambda x: x['date_unix'])[0]
+        
+        # Get rocket details
+        rocket_id = upcoming_launch['rocket']
+        rocket_response = requests.get(f'https://api.spacexdata.com/v4/rockets/{rocket_id}')
+        rocket_response.raise_for_status()
+        rocket = rocket_response.json()
+        rocket_name = rocket['name']
+        
+        # Get launchpad details
+        launchpad_id = upcoming_launch['launchpad']
+        launchpad_response = requests.get(f'https://api.spacexdata.com/v4/launchpads/{launchpad_id}')
+        launchpad_response.raise_for_status()
+        launchpad = launchpad_response.json()
+        launchpad_name = launchpad['name']
+        launchpad_locality = launchpad['locality']
+        
+        # Format date to local time
+        date_local = datetime.datetime.strptime(upcoming_launch['date_local'], '%Y-%m-%dT%H:%M:%S%z')
+        formatted_date = date_local.strftime('%Y-%m-%d %H:%M:%S %Z')
 
-response = requests.get(url)
+        # Print launch details in the required format
+        print(f"{upcoming_launch['name']} ({formatted_date}) {rocket_name} - {launchpad_name} ({launchpad_locality})")
+    
+    except requests.RequestException as e:
+        print(f'An error occurred while making an API request: {e}')
+    except Exception as err:
+        print(f'A general error occurred: {err}')
 
-data = response.json()
-
-print(data['location'])
+if __name__ == '__main__':
+    get_upcoming_launch()

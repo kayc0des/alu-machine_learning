f44687cd1b280ed4328404c13faa9d9e32a5f935
kayc0des <k.boafo@alustudent.com>
2024-06-29 14:15:43 +0200

    upcoming

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -17,10 +17,10 @@ def get_upcoming_launch():
         response = requests.get(url)
         response.raise_for_status()
         launches = response.json()
-        
+
         # Sort launches by date_unix to get the soonest one
         upcoming_launch = sorted(launches, key=lambda x: x['date_unix'])[0]
-        
+
         # Get rocket details
         rocket_id = upcoming_launch['rocket']
         rocket_url = 'https://api.spacexdata.com/v4/rockets/{}'.format(
@@ -29,7 +29,7 @@ def get_upcoming_launch():
         rocket_response.raise_for_status()
         rocket = rocket_response.json()
         rocket_name = rocket['name']
-        
+
         # Get launchpad details
         launchpad_id = upcoming_launch['launchpad']
         launchpad_url = 'https://api.spacexdata.com/v4/launchpads/{}'.format(
@@ -39,18 +39,16 @@ def get_upcoming_launch():
         launchpad = launchpad_response.json()
         launchpad_name = launchpad['name']
         launchpad_locality = launchpad['locality']
-        
-        # Format date to local time
-        date_local = datetime.datetime.strptime(
-            upcoming_launch['date_local'], '%Y-%m-%dT%H:%M:%S%z')
-        formatted_date = date_local.strftime('%Y-%m-%d %H:%M:%S %Z')
+    
+        # Use the date_local directly
+        date_local = upcoming_launch['date_local']
 
         # Print launch details in the required format
         print(
             "{} ({}) {} - {} ({})".format(
-                upcoming_launch['name'], formatted_date, rocket_name,
+                upcoming_launch['name'], date_local, rocket_name,
                 launchpad_name, launchpad_locality))
-    
+
     except requests.RequestException as e:
         print('An error occurred while making an API request: {}'.format(e))
     except Exception as err:

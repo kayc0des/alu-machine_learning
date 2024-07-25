a38521bdaa119355e9d89f76531d9e9e8bc1adc5
kayc0des <k.boafo@alustudent.com>
2024-07-25 10:48:34 +0200

    1-crop

---
+++ pipeline/data_augmentation/1-crop.py
@@ -0,0 +1,28 @@
+# Performs a random crop of an image
+
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def crop_image(image, size):
+    '''
+    Performs a random crop of an image
+    
+    Params:
+        - image: a 3d tf.tensor
+        - size - a tuple of the size of the crop
+        
+    Returns:
+        - Cropped image
+    '''
+    return tf.image.random_crop(
+        image, size=size
+    )
+
+
+if __name__ == '__main__':
+    doggies = tfds.load('stanford_dogs', split='train', as_supervised=True)
+    for image, _ in doggies.shuffle(10).take(1):
+        plt.imshow(crop_image(image, (200, 200, 3)))
+        plt.show()

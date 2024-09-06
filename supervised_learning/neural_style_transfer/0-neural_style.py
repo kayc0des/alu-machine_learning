fe20f6912a5187c0608af53c351e316aaabe27d1
kayc0des <k.boafo@alustudent.com>
2024-09-06 08:51:22 +0200

    string literal fix

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -47,8 +47,8 @@ class NST:
         if not (isinstance(content_image, np.ndarray) and
             len(np.shape(content_image)) == 3 and
             np.shape(content_image)[2] == 3):
-            raise TypeError('
-                    content_image must be a numpy.ndarray with shape (h, w, 3)')
+            raise TypeError(
+                    'content_image must be a numpy.ndarray with shape (h, w, 3)')
 
         # Ensure alpha and beta are non-negative numbers
         if alpha < 0:

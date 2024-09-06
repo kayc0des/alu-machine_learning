b24f7aed2a6523af041731b1abbe2c0ed623df53
kayc0des <k.boafo@alustudent.com>
2024-09-06 13:17:42 +0000

    named model

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -128,7 +128,7 @@ class NST:
         content_outputs = [vgg_model.get_layer(self.content_layer).output]
         model_outputs = style_outputs + content_outputs
 
-        model = tf.keras.models.Model(vgg_model.input, model_outputs)
+        model = tf.keras.models.Model(vgg_model.input, model_outputs, name="model")
         model.trainable = False
 
         return model

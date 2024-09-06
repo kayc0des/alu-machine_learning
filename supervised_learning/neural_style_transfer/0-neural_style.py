1fd7247fd958f3987219db724a6227343e7cfc9f
kayc0des <k.boafo@alustudent.com>
2024-09-06 08:09:41 +0200

    updated 0-neural_style

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -50,10 +50,10 @@ class NST:
             raise TypeError('content_image must be a numpy.ndarray with shape (h, w, 3)')
 
         # Ensure alpha and beta are non-negative numbers
-        if alpha < 0:
+        if alpha <= 0:
             raise TypeError('alpha must be a non-negative number')
 
-        if beta < 0:
+        if beta <= 0:
             raise TypeError('beta must be a non-negative number')
 
         # Instance attributes

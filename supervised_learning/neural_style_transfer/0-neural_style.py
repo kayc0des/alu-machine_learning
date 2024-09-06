4af60f26154d55a16e22a7558642553cd6f5f028
kayc0des <k.boafo@alustudent.com>
2024-09-06 09:47:39 +0200

    tfe

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -60,11 +60,11 @@ class NST:
         if (type(beta) is not float or type(beta) is not int) or beta < 0:
             raise TypeError('beta must be a non-negative number')
 
-        #tf.enable_eager_execution()
+        tf.enable_eager_execution()
 
         # Instance attributes
         self.style_image = self.scale_image(style_image)
-        self.content_image = self.scale(content_image)
+        self.content_image = self.scale_image(content_image)
         self.alpha = alpha
         self.beta = beta
 

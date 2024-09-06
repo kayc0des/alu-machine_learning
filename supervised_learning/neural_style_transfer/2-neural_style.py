12d95c17dc8c893f27ff7317c0837c11c302f856
kayc0des <k.boafo@alustudent.com>
2024-09-06 23:45:38 +0200

    2-neural.style

--- supervised_learning/neural_style_transfer/2-neural_style.py
+++ supervised_learning/neural_style_transfer/2-neural_style.py
@@ -167,8 +167,8 @@ class NST:
             raise TypeError('input_layer must be a tensor of rank 4')
 
         channels = int(input_layer.shape[-1])
-        a = tf.reshape(input_layer, [-1, channels])
-        n = tf.shape(a)[0]
+        a = tf.reshape(input_layer, [1, -1, channels])
+        n = tf.shape(a)[1]
 
         gram = tf.matmul(a, a, transpose_a=True)
         return gram / tf.cast(n, tf.float32)

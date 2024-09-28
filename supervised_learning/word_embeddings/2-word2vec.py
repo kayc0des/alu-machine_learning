136ece3c22c419641de2deb012f5d2b49d7a1d93
kayc0des <k.boafo@alustudent.com>
2024-09-28 18:51:49 +0200

    word2vec

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -18,15 +18,15 @@ def word2vec_model(sentences,
     Creates and trains a gensim vord2vec
     
     Args:
-        - sentences is a list of sentences to be trained on
-        - size is the dimensionality of the embedding layer
-        - min_count is the minimum number of occurrences of a word for use in training
-        - window is the maximum distance between the current and predicted word within a sentence
-        - negative is the size of negative sampling
-        - cbow is a boolean to determine the training type; True is for CBOW; False is for Skip-gram
-        - iterations is the number of iterations to train over
-        - seed is the seed for the random number generator
-        - workers is the number of worker threads to train the model
+        - sentences: list of sentences to be trained on
+        - size: dimensionality of the embedding layer
+        - min_count: minimum number of occurrences of a word for use in training
+        - window: maximum distance between the current and predicted word within a sentence
+        - negative: size of negative sampling
+        - cbow: boolean to determine the training type; True is for CBOW; False is for Skip-gram
+        - iterations: number of iterations to train over
+        - seed: seed for the random number generator
+        - workers: number of worker threads to train the model
     
     Returns:
         Trained model

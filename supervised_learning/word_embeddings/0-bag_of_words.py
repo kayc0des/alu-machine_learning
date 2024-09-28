8697e4431efd5ba8eea0c020d1d8317d4018255f
kayc0des <k.boafo@alustudent.com>
2024-09-28 14:17:14 +0200

    comment off

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -56,17 +56,18 @@ def bag_of_words(sentences, vocab=None):
 
     return embeddings, vocab
 
-# if __name__ == '__main__':
-#     sentences = ["Holberton school is Awesome!",
-#              "Machine learning is awesome",
-#              "NLP is the future!",
-#              "The children are our future",
-#              "Our children's children are our grandchildren",
-#              "The cake was not very good",
-#              "No one said that the cake was not very good",
-#              "Life is beautiful"]
 
-#     E, F = bag_of_words(sentences)
-#     print(E)
-#     print(F)
-#     print(E.shape)
+if __name__ == '__main__':
+    sentences = ["Holberton school is Awesome!",
+             "Machine learning is awesome",
+             "NLP is the future!",
+             "The children are our future",
+             "Our children's children are our grandchildren",
+             "The cake was not very good",
+             "No one said that the cake was not very good",
+             "Life is beautiful"]
+
+    E, F = bag_of_words(sentences)
+    print(E)
+    print(F)
+    print(E.shape)

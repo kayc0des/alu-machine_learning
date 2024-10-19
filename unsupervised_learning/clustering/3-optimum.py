1825baea0e39035ccb4cdc067bc01e6a9c67fcae
kayc0des <k.boafo@alustudent.com>
2024-10-19 18:37:27 +0200

    3

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -11,38 +11,29 @@ variance = __import__('2-variance').variance
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
-    """
-    calculates intra-cluster variance for a dataset
-
-    X: numpy.ndarray (n, d) containing the dataset
-        - n no. of data points
-        - d no. of dimensions for each data point
-    kmin: positive integer - the minimum no. of clusters
-    kmax: positive integer - the maximum no. of clusters
-    iterations: +ve(int) - max no. of iterations perfomed
-
-    return:
-        - results: list containing the results of the
-        K-means for each cluster size
-        - d_vars: list containing the difference in variance
-        from the smallest cluster size for each cluster size
-    """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None
-    if not isinstance(kmin, int) or kmin <= 0:
-        return None, None
-    if not isinstance(kmax, int) or kmax <= 0:
-        return None, None
-    if kmin >= kmax:
-        return None, None
-    if not isinstance(iterations, int) or iterations <= 0:
-        return None, None
-    results = []
-    d_vars = []
+    '''
+    Test for the optimum number of clusters by variance
+    
+    Args:
+        X: numpy.ndarray of shape (n, d)
+        containing the data set
+        kmin: positive integer
+        containing the minimum number of clusters to check for (inclusive)
+        kmax: positive integer
+        containing the maximum number of clusters to check for (inclusive)
+        iterations: positive integer
+        containing the maximum number of iterations for K-means
+    Returns:
+        results: list containing the
+        outputs of K-means for each cluster size
+        d_vars: list containing the difference in
+        variance from the smallest cluster size for each cluster size
+    '''
+    if kmax is None:
+        kmax = X.shape[0]
+    results, d_vars = [], []
     for k in range(kmin, kmax + 1):
         C, clss = kmeans(X, k, iterations)
         results.append((C, clss))
-        if k == kmin:
-            var = variance(X, C)
-        d_vars.append(var - variance(X, C))
-    return results, d_vars
+        d_vars.append(variance(X, C))
+    return results, [d_vars[0] - x for x in d_vars]

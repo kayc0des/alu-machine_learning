03e0ddf1b0308ab249005879ac56e8a12617452c
kayc0des <k.boafo@alustudent.com>
2024-10-19 19:41:23 +0200

    6

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -11,14 +11,15 @@ pdf = __import__('5-pdf').pdf
 def expectation(X, pi, m, S):
     '''
     Expectation step in the EM algorithm for a GMM
-    
+
     Args:
         X (np.ndarray): Data set of shape (n, d)
         pi (np.ndarray): Priors for each cluster of shape (k,)
         m (np.ndarray): Centroid means for each cluster of shape (k, d)
         S (np.ndarray): Covariance matrices for each cluster of shape (k, d, d)
     Returns:
-        g (np.ndarray): Posterior probabilities for each data point in each cluster
+        g (np.ndarray): Posterior probabilities
+        for each data point in each cluster
         l (float): Total log likelihood
     '''
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
@@ -29,10 +30,10 @@ def expectation(X, pi, m, S):
         return None, None
     if not isinstance(S, np.ndarray) or len(S.shape) != 3:
         return None, None
-    
+
     n, d = X.shape
     k = pi.shape[0]
-    
+
     if m.shape[0] != k or m.shape[1] != d:
         return None, None
     if S.shape[0] != k or S.shape[1] != d or S.shape[2] != d:
@@ -47,13 +48,13 @@ def expectation(X, pi, m, S):
             if pdf_result is None:
                 return None, None
             g[i] = pi[i] * pdf_result
-        
+
         total_likelihood = np.sum(g, axis=0)
         if np.any(total_likelihood == 0):
             return None, None
-        
+
         l = np.sum(np.log(total_likelihood))
         g /= total_likelihood
         return g, l
-    except:
+    except Exception as e:
         return None, None

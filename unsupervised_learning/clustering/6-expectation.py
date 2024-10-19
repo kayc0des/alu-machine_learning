b15149f28ed72c997170d9e25e08709ad4b7365d
kayc0des <k.boafo@alustudent.com>
2024-10-19 19:34:48 +0200

    6

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -3,13 +3,33 @@
 Expectation step in the EM algorithm for a GMM
 '''
 
+
 import numpy as np
 pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
-    if not isinstance(X, np.ndarray) or not isinstance(pi, np.ndarray) or not isinstance(m, np.ndarray) or not isinstance(S, np.ndarray):
+    '''
+    Expectation step in the EM algorithm for a GMM
+    
+    Args:
+        X (np.ndarray): Data set
+        pi (np.ndarray): Priors for each cluster
+        m (np.ndarray): Centroid means for each cluster
+        S (np.ndarray): Covariance matrices for each cluster
+    Returns:
+        g (np.ndarray): Posterior probabilities for each data point in each cluster
+        l (float): Total log likelihood
+    '''
+    if not isinstance(X, np.ndarray):
         return None, None
+    if not isinstance(pi, np.ndarray):
+        return None, None
+    if not isinstance(m, np.ndarray):
+        return None, None
+    if not isinstance(S, np.ndarray):
+        return None, None
+
     k, d = m.shape
     n = X.shape[0]
     g = np.zeros((k, n))

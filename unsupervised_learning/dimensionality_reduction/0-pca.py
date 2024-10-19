b3cd418a54eb49414161430225b938276ba1ef3a
kayc0des <k.boafo@alustudent.com>
2024-10-19 17:51:19 +0200

    0 pca

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,36 +1,32 @@
 #!/usr/bin/env python3
-''' Script performs PCA on a dataset '''
+"""Write a function that performs PCA on dataset"""
 
 
 import numpy as np
 
 
 def pca(X, var=0.95):
-    '''
-    Performs PCA on a dataset
+    """
+    PCA on a dataset
 
-    Args:
-        X -> np array of shape (n, d)
-        var -> fraction of the variance that
-            the PCA transformation should maintain
+    parameters:
+        X [numpy.ndarray of shape (n, d)]: dataset
+            n: number of data points
+            d: number of dimensions in each data point
+            all dimensions have a mean of 0 across all data points
+        var [float]: the fraction of the variance that the PCA
+            transformation should maintain
 
-    Returns:
-        W -> Weights matrix of shape (d, nd) that maintains
-            var function of X's ooriginal variance
-        nd -> the new dimensionality of the transformed X
-    '''
-    cov_matrix = np.cov(X, rowvar=False)
-    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
-
-    sorted_idx = np.argsort(eigenvalues)[::-1]
-    sorted_eigenvalues = eigenvalues[sorted_idx]
-    sorted_eigenvectors = eigenvectors[:, sorted_idx]
-
-    explained_variance = sorted_eigenvalues / np.sum(sorted_eigenvalues)
-    cumulative_variance = np.cumsum(explained_variance)
-
-    nd = np.searchsorted(cumulative_variance, var) + 1
-
-    W = sorted_eigenvectors[:, :nd]
-
-    return W
+    returns:
+        W [numpy.ndarray of shape (d, nd)]: the weights matrix that
+            maintains var fraction of X's original variance
+            d: number of dimensions of each data point
+            nd: new dimensionality of the transformed X
+    """
+    # n, d = X.shape
+    u, s, v = np.linalg.svd(X)
+    ratios = list(x / np.sum(s) for x in s)
+    variance = np.cumsum(ratios)
+    nd = np.argwhere(variance >= var)[0, 0]
+    W = v.T[:, :(nd + 1)]
+    return (W)
